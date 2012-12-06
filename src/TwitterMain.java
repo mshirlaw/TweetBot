aa8d9f67ea9abe1b6caa92901a1807d874b696bb
Matt Shirlaw <mshirlaw@yahoo.com>
2012-12-06 21:58:51 +1100

    Adding comments to the TwitterMain and TwitterView classes

--- src/TwitterMain.java
+++ src/TwitterMain.java
@@ -2,7 +2,8 @@ import java.io.IOException;
 import twitter4j.TwitterException;
 
 /**
- * 
+ * The main class creates an intances of the 
+ * TwitterView GUI class and waits for user input.
  * @author mshirlaw
  *
  */
--- src/TwitterView.java
+++ src/TwitterView.java
@@ -7,7 +7,9 @@ import javax.swing.*;
 import twitter4j.TwitterException;
 
 /**
- * 
+ * The TwitterView class creates a simple 
+ * GUI for the Twitter client. This GUI will be expanded over time
+ * as the application is developed further.
  * @author mshirlaw
  *
  */
@@ -25,15 +27,20 @@ public class TwitterView extends JFrame
 	private TwitterManager manager;
 
 	/**
-	 *  
+	 *  The constructor creates a GUI which consists of 
+	 *  a text field and two buttons. The user can enter a tweet 
+	 *  into the text field and clear the text or post it 
+	 *  to twitter.
 	 * @throws IOException
 	 * @throws TwitterException
 	 */
 	public TwitterView() throws IOException, TwitterException
 	{
+		//create an instance of the TwitterManager class and authorise the app
 		manager = new TwitterManager();
 		manager.authorise();	
 
+		//set the size of the window and default close operation
 		setSize(300,200);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
@@ -43,9 +50,11 @@ public class TwitterView extends JFrame
 		mainPanel.setLayout(new GridLayout(3,1));
 		buttonPanel.setLayout(new GridLayout(1,2));
 		
+		//create a label so that the user knows how to use the app
 		label = new JLabel("Enter a tweet or type \"quit\" to exit: ", JLabel.CENTER);
 		textField = new JTextField();
 		
+		//create the tweet button and set an action listener which calls the tweet method
 		tweetButton = new JButton("Send Tweet");
 		tweetButton.addActionListener(new ActionListener() {	   
 			@Override
@@ -69,6 +78,7 @@ public class TwitterView extends JFrame
 			}
 	    }); 
 		
+		//create the clear button and an action listener that resets the text to an empty string
 		clearButton = new JButton("Clear");
 		clearButton.addActionListener(new ActionListener() {	   
 			@Override
